b0bcec2f7e27fccc99a4d6ad1a128b6d60a90a2f
Karakis <emre.karakis@accenture.com>
2017-04-06 00:14:55 +0300

    ConcreteTestCaseClass

--- SoftwareTestingTermProject/src/common/ConcreateTestCase.java
+++ SoftwareTestingTermProject/src/common/ConcreateTestCase.java
@@ -1,5 +1,56 @@
 package common;
 
+import java.util.List;
+
 public class ConcreateTestCase {
+
+	private List<InputVariable<Integer>> inputs;
+	private List<OutputVariable<String>> expectedOutput;
+	private List<OutputVariable<String>> observedOutput;
+	private String testResult;
+
+	public ConcreateTestCase(List<InputVariable<Integer>> inputs, List<OutputVariable<String>> expectedOutputs,
+			List<OutputVariable<String>> observedOutputs) {
+		this.inputs = inputs;
+		this.expectedOutput = expectedOutputs;
+		this.observedOutput = observedOutputs;
+
+	}
+
+	public ConcreateTestCase() {
+
+	}
+
+	public List<InputVariable<Integer>> getInputs() {
+		return inputs;
+	}
+
+	public void setInputs(List<InputVariable<Integer>> inputs) {
+		this.inputs = inputs;
+	}
+
+	public List<OutputVariable<String>> getExpectedOutput() {
+		return expectedOutput;
+	}
+
+	public void setExpectedOutput(List<OutputVariable<String>> expectedOutput) {
+		this.expectedOutput = expectedOutput;
+	}
+
+	public List<OutputVariable<String>> getObservedOutput() {
+		return observedOutput;
+	}
+
+	public void setObservedOutput(List<OutputVariable<String>> observedOutput) {
+		this.observedOutput = observedOutput;
+	}
+
+	public String getTestResult() {
+		return testResult;
+	}
+
+	public void setTestResult(String testResult) {
+		this.testResult = testResult;
+	}
 
 }
--- SoftwareTestingTermProject/src/tab/ConcreteTestCaseRowTab.java
+++ SoftwareTestingTermProject/src/tab/ConcreteTestCaseRowTab.java
@@ -1,70 +1,138 @@
 package tab;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import common.AbstractTestCase;
 import common.ConcreateTestCase;
+import common.Condition;
 import common.DecisionTable;
 import common.ETabType;
+import common.EquivalenceClass;
+import common.InputVariable;
+import common.OutputVariable;
 
 public class ConcreteTestCaseRowTab extends RowTab<ConcreateTestCase> {
-	
-	
+
 	private List<AbstractTestCase> abstractTestCases;
-	
+
 	public ConcreteTestCaseRowTab(ExcelFile excelFile) {
 		// TODO Auto-generated constructor stub
 		this.abstractTestCases = new ArrayList<AbstractTestCase>();
 		super.eTabType = ETabType.ETabType_Concrete_Test_Cases;
 		super.rows = new ArrayList<ConcreateTestCase>();
-		super.hostExcelFile= excelFile;
-		
+		super.hostExcelFile = excelFile;
+
 		read();
+		if(write()){
+			System.out.println("Please check excel file under Output Folder , Its Abstract Test Cases tab was updated.");
+		}
 	}
-	
+
 	@Override
 	public Boolean write() {
-		return null;
+	
+		try {
+			String path = "Output/Homework1_template.xlsx";
+			FileInputStream fileInputStream = new FileInputStream(path);
+			
+			XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+			XSSFSheet sheet = workbook.getSheetAt(7);
+			Row conditionValuesRow = sheet.getRow(2);
+			int testCasesInitialRowIndex = 3;
+			
+			for (AbstractTestCase abstractTestCase : abstractTestCases) {
+				if(abstractTestCase.isSAT()){ // we write just satisfiable conditions
+					ConcreateTestCase concreteTestCase= new ConcreateTestCase();
+					//EquivalenceClass eq = new EquivalenceClass(minValue, maxValue);
+					//our output range is something like that
+					// the triangle is equiliteral,isosceles,scalene,not a triangle
+					List<InputVariable <Integer>> inputVariables = new ArrayList<>();
+					List<OutputVariable<String>> expectedOutput = new ArrayList<>();
+					List<OutputVariable<String>> observedOutput = new ArrayList<>();
+					String testResult= null;
+					
+					//variables are filled here
+					
+                    concreteTestCase.setInputs(inputVariables);
+                    concreteTestCase.setExpectedOutput(expectedOutput);
+                    concreteTestCase.setObservedOutput(observedOutput);
+                    concreteTestCase.setTestResult(testResult);
+		            
+                    // writing Excel File as outputs
+					
+					
+				}
+			}
+			return true;
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (EncryptedDocumentException e) {
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+			return false;
+		}
+
 	}
 
 	/*
-	 * Read from AbstractTestCase tab and determine that
-	 * Which abstract test cases are SAT or not, to determine SAT or not
-	 * we are going to trust user input for now we could not implement SAT4J 
-	 * on project.
-	 * */
+	 * Read from AbstractTestCase tab and determine that Which abstract test
+	 * cases are SAT or not, to determine SAT or not we are going to trust user
+	 * input for now we could not implement SAT4J on project.
+	 */
 	@Override
 	public Boolean read() {
 		// TODO Auto-generated method stub
 		List<Tab> tabs = new ArrayList<Tab>();
-		//List<AbstractTestCase> abstractTestCases = new ArrayList<AbstractTestCase>();
-		tabs= hostExcelFile.getTabs();
+		// List<AbstractTestCase> abstractTestCases = new
+		// ArrayList<AbstractTestCase>();
+		tabs = hostExcelFile.getTabs();
 		for (Tab tab : tabs) {
-			if(tab.eTabType == ETabType.ETabType_Abstract_Test_Cases){
+			if (tab.eTabType == ETabType.ETabType_Abstract_Test_Cases) {
 				AbstractTestCaseRowTab _tab = (AbstractTestCaseRowTab) tab;
 				this.abstractTestCases = _tab.getRows();
-				
+
 			}
 		}
 		for (AbstractTestCase abstractTestCase : this.abstractTestCases) {
 			System.out.println(abstractTestCase.toString());
-			//ask user on console that Is given Abstract Test Case is SAT or Not.
-			while(true){
+			// ask user on console that Is given Abstract Test Case is SAT or
+			// Not.
+
+			while (true) {
 				System.out.println("Given Condition set is Satisfiable or not ,please enter Y or N");
 				Scanner scan = new Scanner(System.in);
 				String enteredMessage = scan.nextLine();
-				if(enteredMessage.equalsIgnoreCase("Y")){
-					//make abstract test case as SAT
-					abstractTestCase.setSAT(true);
-					
-				}else if (enteredMessage.equalsIgnoreCase("N")){
-					//make abstract test as not SAT
-					abstractTestCase.setSAT(false);
-				}	
+				if (!enteredMessage.equals("") || enteredMessage != null) {
+					if (enteredMessage.equalsIgnoreCase("Y") || enteredMessage.equalsIgnoreCase("y")) {
+						// make abstract test case as SAT
+						abstractTestCase.setSAT(true);
+						break;
+
+					} else if (enteredMessage.equalsIgnoreCase("N") || enteredMessage.equalsIgnoreCase("n")) {
+						// make abstract test as not SAT
+						abstractTestCase.setSAT(false);
+						break;
+					}
+				}
 			}
-			
+
 		}
 		return null;
 	}
